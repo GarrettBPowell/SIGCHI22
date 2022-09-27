d07af43c72cd50c8f21cd61d4948f5a814aab668
Garrett Powell <garettbpowell@gmail.com>
2022-09-27 17:57:18 -0500

    Added more to menu. Added splitGifs to gitignore

--- GoogleVisionAPI/imageAna.py
+++ GoogleVisionAPI/imageAna.py
@@ -3,10 +3,20 @@ from os import listdir
 from os.path import isfile, join
 import io
 import os
+import datetime
 
-def getFrames(fileName, gif, num_key_frames):
+def getXAmountOfFrames(fileName, gif, num_key_frames):
     with Image.open(gif) as im:
         for i in range(num_key_frames):
             im.seek(im.n_frames // num_key_frames * i)
             im.save(os.path.abspath('resources/splitGifs/{}-{}.png'.format(fileName, i)))
-            print(i)
+
+def getFirstAndMiddleFrame(fileName, gif):
+    ct = datetime.datetime.now()
+    with Image.open(gif) as im:
+        im.seek(0)
+        im.save(os.path.abspath('resources/splitGifs/{}-{}-first-{}.png'.format(fileName, 0, ct.timestamp())))
+
+        midPoint = im.n_frames // 2
+        im.seek(midPoint)
+        im.save(os.path.abspath('resources/splitGifs/{}-{}-middle-{}.png'.format(fileName, midPoint, ct.timestamp())))
--- GoogleVisionAPI/main.py
+++ GoogleVisionAPI/main.py
@@ -1,6 +1,7 @@
 import GoogleVisionAPI as GV
 import imageAna as IA
 import os
+from PIL import Image
 ####################
 # main             #
 #########################################
@@ -8,27 +9,68 @@ import os
 # set GOOGLE_APPLICATION_CREDENTIALS=   #
 #########################################
 
-# menu
-print("What would you like to run?")
-print("1. Labels")
-print("2. Run All")
-print("3. Chop Gif")
+runMenu = True
+options = ["Labels", "Run All in pictures folder", "Chop Gif (first and middle frame)", "Split gif into n frames", "Exit"]
+while runMenu:
+    # menu
+    print("\n\n\n\n*******************************************")
+    print("What would you like to run?")
+    print("------------------------------")
+    for i in range(len(options)):
+        print("{}. {}".format(i + 1, options[i]))
+    print("*******************************************")
 
-# input for menu
-print("\n")
-choice = input ("Enter an option: ")
+    # input for menu
+    print("\n")
+    choice = input ("Enter an option: ")
+    print("---------------")
+
+    # menu execution 
+
+    # Single file
+    if(choice == '1'):
+        imageName = input("Enter image/gif name (NEED file extension): ")
+        GV.labels(imageName)
+        print("\n**********\n for {} complete\n**********\n".format(imageName))
+
+    # All files in folder that produces csv with tags
+    elif(choice == '2'):
+        csvName = input("Enter a name for CSV: ")
+        GV.folder(csvName)
+        print("\n**********\n{}.csv was saved in the CSV folder\n**********\n".format(csvName))
+
+    # Split single gif into first and middle frame
+    elif(choice == '3'):
+        imageName = input("Enter gif name (no file extension): ")
+        fileName = os.path.abspath('resources/pictures/' + imageName + '.gif')
+        IA.getFirstAndMiddleFrame(imageName, fileName)
+        print("\n**********\nFirst and Middle frames saved in splitGifs folder\n**********\n")
+
+     # Split single gif into first and middle frame
+    elif(choice == '4'):
+        
+        imageName = input("Enter gif name (no file extension): ")
+        fileName = os.path.abspath('resources/pictures/' + imageName + '.gif')
+        im = Image.open(fileName)
 
 
-# menu execution 
-if(choice == '1'):
-    imageName = input("Enter image name:")
-    GV.labels(imageName)
+        maxFrames = im.n_frames
+        numFrames = maxFrames
+        while(numFrames >= maxFrames):
+            numFrames = int(input("Enter the number of frames you want (Max frames is {}): ".format(maxFrames)))
+            if(numFrames >= maxFrames):
+                print("Input too high. Enter another number")
 
-elif(choice == '2'):
-    csvName = input("Enter a name for CSV: ")
-    GV.folder(csvName)
+        IA.getXAmountOfFrames(imageName, fileName, numFrames)
+        print("\n**********")
+        print("{} frames from {}".format(numFrames, imageName))
+        print("**********\n")
 
-elif(choice == '3'):
-    imageName = input("Enter image name:")
-    file_name = os.path.abspath('resources/pictures/' + imageName)
-    IA.getFrames(imageName, file_name, 4)
+    # Exit
+    elif(choice == str(len(options))):
+        runMenu = False
+
+    # Catch all
+    else:
+        print("\n**********\nOption not available\n**********\n")
+
